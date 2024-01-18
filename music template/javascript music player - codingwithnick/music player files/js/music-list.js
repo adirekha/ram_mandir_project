b736344ff33f6f63e88cdf88977adf12285c65c4
adirekha <152973043+adirekha@users.noreply.github.com>
2024-01-18 22:23:08 +0530

    LFS

---
+++ music template/javascript music player - codingwithnick/music player files/js/music-list.js
@@ -0,0 +1,49 @@
+let allMusic = [
+
+    {
+        name: "Ekadantaya Vakratundaya Gauri  ",
+        artist: "jai ganesh ji",
+        img: "ganesh",
+        src: "Ekadantaya Vakratundaya Gauri Tanaya Dhimi _ Full Song with Lyrics _ Shankar Mahadevan(MP3_320K)"
+    },
+    {
+        name: "Aigiri Nandini",
+        artist: "Audio Library",
+        img: "matarani",
+        src: "Aigiri Nandini With Lyrics _ Mahishasura Mardini _ Rajalakshmee Sanjay _ महिषासुर मर्दिनी स्तोत्र(MP3_320K)"
+    },
+    {
+        name: "JAI SHREE RAM",
+        artist: "aditya raj",
+        img: "ramjiphoto",
+        src: "Jai Shree Ram _ Hansraj Raghuwanshi _ Ayodhya Ram Mandir Song 2024 _ Yug Ram Raj Ka(MP3_320K)"
+    },{
+        name: "Namo Namo",
+        artist: "kedarnath ji",
+        img: "sankar",
+        src: "Namo Namo - Lyrical _ Kedarnath _ Sushant Rajput _ Sara Ali Khan _ Amit Trivedi _ Amitabh B(MP3_160K)"
+    },
+    {
+        name: "Parvati Boli Shankar Se",
+        artist: "jai bholenath ji",
+        img: "bholenathji",
+        src: "Parvati Boli Shankar Se - O Bholenath Ji _ Hansraj Raghuwanshi _ Full Song _ Bhole Baba Song(MP3_160K)"
+    },
+    {
+        name: "Shiv Tandav Stotram",
+        artist: "aditya raj",
+        img: "shankarbhagwan",
+        src: "Shiv Tandav Stotram _ Shankar Mahadevan _ रावण रचित शिव तांडव स्तोत्र(MP3_320K)"
+    },
+    {
+        name:"SHRI KRISHNA",
+        artist: "aditya",
+        img: "music-7",
+        src:"Sri Ram Flute Ringtone 2021 __ Mahabharat Them Ringtone __ BM Shorts __(MP3_320K)",
+
+
+    }
+
+
+
+    ];
